07f5239cd75e1c03b4c49b36849fcf25ca39aeae
Ioritz <iperugoald1@educacion.navarra.es>
2024-01-03 17:19:18 +0100

    Creacion de la clase "Jugador". Atributos y Metodos

---
+++ src/Jugador.java
@@ -0,0 +1,37 @@
+public class Jugador {
+    private String Nombre;
+    private String Nacionalidad;
+    private int edad;
+    private String Posicion;
+    private boolean Lesionado;
+
+    public Jugador(String Nombre, String Nacionalidad, int edad, String Posicion){
+        this.Nombre = Nombre;
+        this.Nacionalidad = Nacionalidad;
+        this.edad = edad;
+        this.Lesionado = false;
+        this.Posicion = Posicion;
+    }
+
+    public String getNombre(){
+        return Nombre;
+    }
+    public String getNacionalidad(){
+        return Nacionalidad;
+    }
+    public int getEdad(){
+        return edad;
+    }
+    public String getPosicion(){
+        return Posicion;
+    }
+    public boolean getLesionado(){
+        return Lesionado;
+    }
+    public void Lesionarse(){
+        Lesionado = true;
+    }
+    public void Recuperarse(){
+        Lesionado = false;
+    }
+}
